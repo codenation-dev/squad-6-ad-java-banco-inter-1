b9337aa065cf4b6d59aa462c8de6675ebce33e4f
Raul F. M <raulfmansur@hotmail.com>
2019-12-05 18:01:24 -0300

    Bug fix nas rotas sem autenticação

--- src/main/java/br/com/codenation/aceleradev/security/SecurityConfig.java
+++ src/main/java/br/com/codenation/aceleradev/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/usuario**", "/h2-console**");
+        web.ignoring().antMatchers("/usuario/**", "/h2-console/**");
     }
 
     @Autowired
