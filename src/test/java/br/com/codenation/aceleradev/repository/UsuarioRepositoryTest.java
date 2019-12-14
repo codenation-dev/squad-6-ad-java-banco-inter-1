e369563b87d6ab8cdc5a18a93fdd943afd3215f3
Arky005 <otavio.n.morais@hotmail.com>
2019-12-14 13:25:35 -0300

    Testes de inserção do UsuarioRepository e ErroRepository

--- src/test/java/br/com/codenation/aceleradev/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/codenation/aceleradev/repository/UsuarioRepositoryTest.java
@@ -1,34 +1,48 @@
 package br.com.codenation.aceleradev.repository;
 
+import br.com.codenation.aceleradev.domain.Usuario;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
 
-
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    private Usuario usuario;
+
     @Before
     public void setUp(){
-
+        usuario = new Usuario();
+        usuario.setEmail("teste_repository@squad6.com.br");
+        usuario.setNome("Teste repository");
+        usuario.setSenha(new BCryptPasswordEncoder().encode("senhateste"));
+        usuario.setToken("batata");
+        usuarioRepository.save(usuario);
     }
 
     @Test
-    public void testExemplo(){
-
+    public void testInsercaoDeUsuario(){
+        assertEquals(usuario, usuarioRepository.findByEmail("teste_repository@squad6.com.br").get());
     }
 
     @After
     public void tearDown(){
-        this.usuarioRepository.deleteAll();
+        usuarioRepository.delete(usuario);
     }
 
 }
