7ad8e6a853c0ddd9a8bc1fe92454226fe0605469
Arky005 <otavio.n.morais@hotmail.com>
2019-12-16 11:28:23 -0300

    Teste do UsuarioController

--- src/test/java/br/com/codenation/aceleradev/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/codenation/aceleradev/controller/UsuarioControllerTest.java
@@ -5,10 +5,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -19,21 +27,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class UsuarioControllerTest {
 
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
     @Before
     public void setUp(){
-
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
-    public void test() throws URISyntaxException {
-        RestTemplate restTemplate = new RestTemplate();
-
-        final String baseUrl = "http://google.com";
-        URI uri = new URI(baseUrl);
-
-        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-
-        assertEquals(result.getStatusCodeValue(), 200);
+    public void testAcessoEndpointUsuario() throws Exception {
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get("/usuario/1111")
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
     }
 
 
