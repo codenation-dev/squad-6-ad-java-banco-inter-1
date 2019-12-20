c53d6214585bcd337fcaa05b0b8133b9c60ec570
Raul F. M <raulfmansur@hotmail.com>
2019-12-20 14:33:12 -0300

    Inclusão de frequência de erros na busca por ambiente

--- src/main/java/br/com/codenation/aceleradev/repository/ErroRepository.java
+++ src/main/java/br/com/codenation/aceleradev/repository/ErroRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -28,17 +29,33 @@ public interface ErroRepository extends JpaRepository<Erro, Long> {
             " erro1.titulo," +
             " erro1.usuario.id," +
             " u.token," +
-            " count(erro2.id))  " +
+            " count(erro2.id)) " +
             " FROM Erro erro1 JOIN Erro erro2 ON erro1.titulo = erro2.titulo" +
             " INNER JOIN Usuario u ON u.id = erro1.usuario.id" +
-            " GROUP BY erro1.id")
+            " GROUP BY erro1.id, u.token")
     Page<ErroDTO> findAllErroDTO(Pageable pageable);
 
     Page<Erro> findByLevel(Pageable pageable, LevelEnum level);
 
     Page<Erro> findByUsuarioId(Pageable pageable, Long usuarioId);
 
-    Page<Erro> findByAmbiente(Pageable pageable, AmbienteEnum ambienteEnum);
+    @Query(value = "SELECT new br.com.codenation.aceleradev.dto.ErroDTO(" +
+            " erro1.id," +
+            " erro1.createAt," +
+            " erro1.ambiente," +
+            " erro1.data," +
+            " erro1.detalhes," +
+            " erro1.endereco," +
+            " erro1.level," +
+            " erro1.status," +
+            " erro1.titulo," +
+            " erro1.usuario.id," +
+            " u.token," +
+            " count(erro2.id)) " +
+            " FROM Erro erro1 JOIN Erro erro2 ON erro1.titulo = erro2.titulo AND erro2.ambiente = :ambiente" +
+            " INNER JOIN Usuario u ON u.id = erro1.usuario.id" +
+            " GROUP BY erro1.id, u.token")
+    Page<Erro> findByAmbiente(Pageable pageable, @Param("ambiente") AmbienteEnum ambienteEnum);
 
     Page<Erro> findByAmbienteAndTitulo(Pageable pageable, AmbienteEnum ambiente, String titulo);
 
